f68933eb6292d03d3e5567b7785ca170b4b510d4
Jesús Urrutia <jesus.urrutia@gmail.com>
2022-05-14 13:46:26 -0400

    feat: replace redis-store with ioredis

--- apps/accounts/src/app.module.ts
+++ apps/accounts/src/app.module.ts
@@ -7,7 +7,7 @@ import {
   Module,
 } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import * as redisStore from 'cache-manager-redis-store';
+import * as redisStore from 'cache-manager-ioredis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsListener } from './listeners/posts.listener';
@@ -17,6 +17,7 @@ import { PostsListener } from './listeners/posts.listener';
     EventEmitterModule.forRoot(),
     CacheModule.register({
       store: redisStore,
+      enableOfflineQueue: false,
     }),
     JsonPlaceHolderModule.forRoot({
       baseUrl: 'https://jsonplaceholder.typicode.com',
@@ -29,11 +30,13 @@ export class AppModule {
   private readonly logger: Logger = new Logger(AppModule.name);
 
   constructor(@Inject(CACHE_MANAGER) private readonly cacheManager) {
-    const client = cacheManager.store.getClient();
+    const client = cacheManager.store?.getClient?.();
 
     // Prevent app crash when redis server is unavailable
-    client.on('error', (err) => {
-      this.logger.error(err);
-    });
+    if (client) {
+      client.on('error', (err) => {
+        this.logger.error(err);
+      });
+    }
   }
 }
