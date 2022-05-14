9f24ce1941cf4766b3a8ed89058fa1bc8ffbe617
Jesús Urrutia <jesus.urrutia@gmail.com>
2022-05-14 11:37:12 -0400

    feat: setting redis-store

--- apps/accounts/src/app.module.ts
+++ apps/accounts/src/app.module.ts
@@ -1,6 +1,13 @@
 import { JsonPlaceHolderModule } from '@app/json-place-holder';
-import { CacheModule, Module } from '@nestjs/common';
+import {
+  CacheModule,
+  CACHE_MANAGER,
+  Inject,
+  Logger,
+  Module,
+} from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import * as redisStore from 'cache-manager-redis-store';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsListener } from './listeners/posts.listener';
@@ -8,7 +15,9 @@ import { PostsListener } from './listeners/posts.listener';
 @Module({
   imports: [
     EventEmitterModule.forRoot(),
-    CacheModule.register(),
+    CacheModule.register({
+      store: redisStore,
+    }),
     JsonPlaceHolderModule.forRoot({
       baseUrl: 'https://jsonplaceholder.typicode.com',
     }),
@@ -16,4 +25,15 @@ import { PostsListener } from './listeners/posts.listener';
   controllers: [AppController],
   providers: [AppService, PostsListener],
 })
-export class AppModule {}
+export class AppModule {
+  private readonly logger: Logger = new Logger(AppModule.name);
+
+  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager) {
+    const client = cacheManager.store.getClient();
+
+    // Prevent app crash when redis server is unavailable
+    client.on('error', (err) => {
+      this.logger.error(err);
+    });
+  }
+}
