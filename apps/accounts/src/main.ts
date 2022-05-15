8099020bed1e910b707ed1999638744197702b7f
Jesús Urrutia <jesus.urrutia@gmail.com>
2022-05-14 20:32:03 -0400

    feat: support set log level from environment vars

--- apps/accounts/src/main.ts
+++ apps/accounts/src/main.ts
@@ -1,8 +1,25 @@
+import { LogLevel } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // -- Determine logger level
+  const defaultLoggerLevel: LogLevel = 'log';
+  const loggerLevels: LogLevel[] = ['error', 'warn', 'log', 'debug', 'verbose'];
+  const isLoggerEnabled = process.env.LOG_DISABLE === undefined;
+  const rawLoggerLevel = (
+    process.env.LOG_LEVEL ?? ''
+  ).toLowerCase() as LogLevel;
+  const loggerLevel = loggerLevels.includes(rawLoggerLevel)
+    ? rawLoggerLevel
+    : defaultLoggerLevel;
+  const logger =
+    isLoggerEnabled &&
+    loggerLevels.slice(0, loggerLevels.indexOf(loggerLevel) + 1);
+
+  const app = await NestFactory.create(AppModule, {
+    logger,
+  });
 
   app.enableCors();
 
